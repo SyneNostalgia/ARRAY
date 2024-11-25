564cf2d5a3a381d973dc5993c09798306147dad9
SyneNostalgia <achitathenew@gmail.com>
2024-11-25 13:11:35 +0700

    Add project files.

---
+++ ARRAY/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ARRAY
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            comboBox1 = new ComboBox();
+            listBox1 = new ListBox();
+            checkedListBox1 = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(46, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Array1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(46, 188);
+            button2.Name = "button2";
+            button2.Size = new Size(198, 57);
+            button2.TabIndex = 1;
+            button2.Text = "Array2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(309, 53);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(199, 331);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.IndianRed;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(635, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(209, 331);
+            label1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(514, 118);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 42);
+            button3.TabIndex = 4;
+            button3.Text = "GetData";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(309, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 5;
+            label2.Text = "พิมพ์รายชื่อ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(635, 33);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 20);
+            label3.TabIndex = 6;
+            label3.Text = "รายชื่อที่บันทึกแล้ว";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(514, 203);
+            button4.Name = "button4";
+            button4.Size = new Size(115, 42);
+            button4.TabIndex = 7;
+            button4.Text = "GetNumber";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(46, 327);
+            button5.Name = "button5";
+            button5.Size = new Size(198, 57);
+            button5.TabIndex = 8;
+            button5.Text = "Array3";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(878, 82);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 9;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(879, 141);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 104);
+            listBox1.TabIndex = 10;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(878, 270);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(150, 114);
+            checkedListBox1.TabIndex = 11;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1055, 411);
+            Controls.Add(checkedListBox1);
+            Controls.Add(listBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+        private Button button4;
+        private Button button5;
+        private ComboBox comboBox1;
+        private ListBox listBox1;
+        private CheckedListBox checkedListBox1;
+    }
+}
---
+++ ARRAY/Form1.cs
@@ -0,0 +1,127 @@
+﻿namespace ARRAY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] student = new string[5];
+            student[0] = "KIM";
+            student[1] = "IQ";
+            student[2] = "TATAH";
+            student[3] = "NYONG";
+            student[4] = "MICHAEL";
+            string test = "";
+            int n = student.Length;
+            for (int i = 0; i < n; i++)
+            {
+                test += student[i] + Environment.NewLine;
+            }
+            MessageBox.Show(test);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] Fruits = { "BANANA", "APPLE", "GRAPE", "ORANGE", "Melon", "Gomu Gomu no Mi" };
+            string myFruit = "";
+            foreach (string str in Fruits)
+            {
+                myFruit += str + ", ";
+            }
+            MessageBox.Show(myFruit, "My fruits");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] data = textBox1.Lines; //นำข้อมูลแต่ละบรรทัดไปใส่ใน array
+            string result = "";
+            foreach (string str in data)
+            {
+                if (str != "")
+                {
+                    result += str + Environment.NewLine;
+                }
+
+            }
+            label1.Text = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] data = textBox1.Lines;
+            string result = "";
+            int sum = 0;
+            foreach (var str in data)
+            {
+                int x = 0;
+                if (int.TryParse(str, out x))
+                {
+                    result += x.ToString() + Environment.NewLine;
+                    sum += x;
+                }
+            }
+            label1.Text = result;
+            MessageBox.Show(sum.ToString(), "THE SUM");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[,] student = new string[3, 2];
+            student[0, 0] = "OPER";
+            student[0, 1] = "BUNGKAN";
+            student[1, 0] = "TATAH";
+            student[1, 1] = "UDON";
+            student[2, 0] = "M";
+            student[2, 1] = "NONGBUALUMPHU";
+            string ResultData = "";
+            int m = student.GetLength(0);
+            int n = student.GetLength(1);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    ResultData += student[i, j];
+                    if (j < n - 1)
+                    {
+                        ResultData += "-";
+                    }
+                }
+                ResultData += Environment.NewLine;
+            }
+            MessageBox.Show(ResultData, "Show data in array 2D");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Fruits = { "BANANA", "APPLE", "GRAPE", "ORANGE", "Melon", "Gomu Gomu no Mi" };
+            comboBox1.Items.AddRange(Fruits);
+            listBox1.Items.AddRange(Fruits);
+            checkedListBox1.Items.AddRange(Fruits);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = comboBox1.SelectedIndex.ToString();
+            str += comboBox1.SelectedItem;
+            MessageBox.Show(str);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = listBox1.SelectedIndex.ToString();
+            str += listBox1.SelectedItem;
+            MessageBox.Show(str);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = checkedListBox1.SelectedIndex.ToString();
+            str += checkedListBox1.SelectedItem;
+            MessageBox.Show(str);
+        }
+    }
+}
